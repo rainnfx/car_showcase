e20f406b107951fd45e0dcedd24857520fe1d266
rainnfx <matteo.aure@gmail.com>
2023-07-28 11:19:26 -0700

    added assets, finishing nav and footer.

--- components/custombutton.tsx
+++ components/custombutton.tsx
@@ -5,11 +5,11 @@ import Image from 'next/image'
 import { CustomButtonProps } from '@/types';
 
 const custombutton = ({ title, containerStyles,
-handleClick }: CustomButtonProps) => {
+handleClick, btnType }: CustomButtonProps) => {
   return (
     <button
         disabled={false}
-        type={"button"}
+        type={btnType || "button"}
         className={`custom-btn ${containerStyles}`}
         onClick={handleClick}
     >
--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,11 @@
 import Hero from './hero';
 import CustomButton from './custombutton';
+import Navbar from './navbar';
+import Footer from './footer';
 
 export {
     Hero,
     CustomButton,
+    Navbar,
+    Footer,
 }
