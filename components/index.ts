dff0b441584981a7aeaca3900cced500713a6a05
rainnfx <matteo.aure@gmail.com>
2023-07-30 11:39:26 -0700

    added stuff

--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,16 @@ import Hero from './hero';
 import CustomButton from './custombutton';
 import Navbar from './navbar';
 import Footer from './footer';
+import SearchBar from './searchbar';
+import CustomFilter from './customfilter';
+import SearchManufacturer from './searchmanufacturer';
 
 export {
     Hero,
     CustomButton,
     Navbar,
     Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
 }
