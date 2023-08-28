1a1d7b0b39395d5db7637870baa6912b8446ccd3
rainnfx <matteo.aure@gmail.com>
2023-08-28 12:04:49 -0700

    stopped procrastinating, I did.

--- components/searchmanufacturer.tsx
+++ components/searchmanufacturer.tsx
@@ -1,55 +1,35 @@
-"use client";
+"use client"
 
-import { useState, Fragment } from 'react';
-import Image from 'next/image';
+import { useState } from 'react';
 import { Combobox, Transition } from '@headlessui/react'
-
-import { manufacturers } from '@/constants';
 import { SearchManufacturerProps } from '@/types'
+import Image from 'next/image'
 
-const SearchManufacturer = ({ manufacturer,
-    setManufacturer}: SearchManufacturerProps) => {
-        const [query, setQuery] = useState('')
-
-        const filteredManufacturers = query === ''
-        ? manufacturers 
-        : manufacturers.filter((item) => (
-            item.toLowerCase()
-            .replace(/\s+/g, "")
-            .includes(query.toLowerCase().replace(/\s+/g,"")
-        )))
-
+const SearchManufacturer = ({ manufacturer, setManufacturer}: SearchManufacturerProps) => {
+    const [query, setquery] = useState('')
   return (
     <div className="search-manufacturer">
         <Combobox>
             <div className="relative w-full">
                 <Combobox.Button className="absolute top-[14px]">
                     <Image
-                        src="/car-logo.svg"
-                        width={20}
-                        height={20}
-                        className="ml-4"
-                        alt="Car Logo"
+                    src="/car-logo.svg"
+                    width={20}
+                    height={20}
+                    className="ml-4"
+                    alt="Car Logo"
                     />
                 </Combobox.Button>
 
-                <Combobox.Input 
-                    className="search-manufacturer__input"
-                    placeholder="Volkswagen"
-                    displayValue={(manufacturer: string) => manufacturer}
-                    onChange={(e) => setQuery(e.target.value)}
+                <Combobox.Input
+                className="search-manufacturer__input"
+                placeholder="Volkswagen"
+                displayValue={(manufacturer: string) => manufacturer}
+                onChange={(e) => setquery(e.target.value)}
                 />
 
-                <Transition
-                as={Fragment}
-                Leave="transition ease-in duration-100"
-                LeaveFrom="opacity-100"
-                LeaveTo="opacity-0"
-                afterLeave={() => setQuery('')}
-                >
-                    <Combobox.Options>
-
-                    </Combobox.Options>
+                <Transition>
+                    {/* video timestamp: 1:03:02, if lost. */}
                 </Transition>
             </div>
         </Combobox>
