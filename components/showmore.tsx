3ea9cb52e0b50d59072b3494202508de238623bb
rainnfx <matteo.aure@gmail.com>
2023-09-11 15:53:32 -0700

    starting on the add more button

---
+++ components/showmore.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { ShowMoreProps } from "@/types";
+import { useRouter } from "next/navigation";
+
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  return (
+    <div>
+      <div>hello me, timestamp: 2:54:44</div>
+    </div>
+  );
+};
+
+export default ShowMore;
