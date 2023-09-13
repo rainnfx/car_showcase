1751ab862f469591e81c49a59d97f46973c0521c
rainnfx <matteo.aure@gmail.com>
2023-09-13 16:17:27 -0700

    showmore works now.

--- components/showmore.tsx
+++ components/showmore.tsx
@@ -2,11 +2,29 @@
 
 import { ShowMoreProps } from "@/types";
 import { useRouter } from "next/navigation";
+import CustomButton from "./custombutton";
+import { updateSearchParams } from "@/utils";
 
 const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+    const newPathName = updateSearchParams("limit", `${newLimit}`);
+
+    router.push(newPathName);
+  };
+
   return (
-    <div>
-      <div>hello me, timestamp: 2:54:44</div>
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          title="Show More"
+          btnType="button"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
     </div>
   );
 };
